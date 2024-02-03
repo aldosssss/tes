1f08a2ee53a6d3ed4c8ed92cd7a497d98922698d
Aldo Stepanus Kaban <133597774+aldosssss@users.noreply.github.com>
2024-02-03 15:45:06 +0700

    tes kamera

--- tugas-akhir.py
+++ tugas-akhir.py
@@ -1,1 +1,10 @@
-tes
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Webcam', frame)
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
